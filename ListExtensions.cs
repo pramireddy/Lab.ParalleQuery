73c9b001b4a9d46dc4e9fc85a5918d9945d3a3d8
Prasad Ramireddy <pramireddy08@gmail.com>
2024-06-28 12:44:48 +0100

    Add project files.

---
+++ ListExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.ParalleQuery
+{
+    public static class ListExtensions
+    {
+        public static IEnumerable<TSource[]> Chunk2<TSource>(this IEnumerable<TSource> source, int size)
+        {
+            return ChunkIterator(source, size);
+        }
+        private static IEnumerable<TSource[]> ChunkIterator<TSource>(IEnumerable<TSource> source, int size)
+        {
+            using IEnumerator<TSource> e = source.GetEnumerator();
+
+            // Before allocating anything, make sure there's at least one element.
+            if (e.MoveNext())
+            {
+                // Now that we know we have at least one item, allocate an initial storage array. This is not
+                // the array we'll yield.  It starts out small in order to avoid significantly overallocating
+                // when the source has many fewer elements than the chunk size.
+                int arraySize = Math.Min(size, 4);
+                int i;
+                do
+                {
+                    var array = new TSource[arraySize];
+
+                    // Store the first item.
+                    array[0] = e.Current;
+                    i = 1;
+
+                    if (size != array.Length)
+                    {
+                        // This is the first chunk. As we fill the array, grow it as needed.
+                        for (; i < size && e.MoveNext(); i++)
+                        {
+                            if (i >= array.Length)
+                            {
+                                arraySize = (int)Math.Min((uint)size, 2 * (uint)array.Length);
+                                Array.Resize(ref array, arraySize);
+                            }
+
+                            array[i] = e.Current;
+                        }
+                    }
+                    else
+                    {
+                        // For all but the first chunk, the array will already be correctly sized.
+                        // We can just store into it until either it's full or MoveNext returns false.
+                        TSource[] local = array; // avoid bounds checks by using cached local (`array` is lifted to iterator object as a field)
+                        Debug.Assert(local.Length == size);
+                        for (; (uint)i < (uint)local.Length && e.MoveNext(); i++)
+                        {
+                            local[i] = e.Current;
+                        }
+                    }
+
+                    if (i != array.Length)
+                    {
+                        Array.Resize(ref array, i);
+                    }
+
+                    yield return array;
+                }
+                while (i >= size && e.MoveNext());
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lab.ParalleQuery;
+using System.Collections.Concurrent;
+
+var cancellationTokenSource = new CancellationTokenSource();
+var token = cancellationTokenSource.Token;
+var degreeOfParallelism = Math.Min(Environment.ProcessorCount, 512);
+
+var threadsMap = new ConcurrentDictionary<int, List<int>>();
+
+int[] Ids = Enumerable.Range(1, 100).ToArray();
+
+var batches = Ids.Chunk2(50).ToArray();
+
+foreach (var batch in batches)
+{
+    batch.AsParallel()
+    .WithDegreeOfParallelism(degreeOfParallelism)
+    .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+    .Where(_ => !token.IsCancellationRequested)
+    .Select(HighComputeProcess).ToArray();
+
+    foreach (var threadMap in threadsMap)
+    {
+        var values = string.Join(",", threadMap.Value.Select(x => x.ToString()));
+        Console.WriteLine($"ThreadId: {threadMap.Key}, RecordCount:{threadMap.Value.Count}, IDsList:{values}");
+    }
+}
+
+
+
+
+int HighComputeProcess(int n)
+{
+    Thread.Sleep(n);
+
+    threadsMap.AddOrUpdate(key: Environment.CurrentManagedThreadId,
+        addValue: new List<int> { n},
+        updateValueFactory: (key,values) => { values.Add(n);return values; });
+    var m = 0;
+    for (int i = 0; i < 1000000; i++)
+    {
+        m++;
+    }
+    return m;
+}
+
+Console.WriteLine("Lab, ParallelQuert!");
+
+Console.ReadLine();
